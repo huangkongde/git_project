24b172dee18eadcf5b1f7ac0e93c41fe0ae39e6d
2686672904@qq.com <2686672904@qq.com>
2020-06-09 15:50:13 +0800

    提交到仓库

---
+++ student_prc/src/com/student/model/StudentModel.java
@@ -0,0 +1,19 @@
+package com.student.model;
+
+public class StudentModel {
+	private int id;
+	private String username;
+	private String upassword;
+	private int sex;
+	private String love;
+	public StudentModel(int id, String username, String upassword, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.upassword = upassword;
+		this.sex = sex;
+		this.love = love;
+	}
+	
+	
+}
